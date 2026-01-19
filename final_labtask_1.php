2d9f5d1d97df7782a0d07c1f5ec722e70ccb2826
sahamonish-crypto <sahamonish4@gmail.com>
2026-01-20 02:15:50 +0600

    Add files via upload

---
+++ final_labtask_1.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = "Apple";
+$age = 20;
+$price = 15.75;
+$isAvailable = true;
+
+$a = 10;
+$b = 5;
+
+echo $a + $b . "<br>";
+echo $a - $b . "<br>";
+echo $a * $b . "<br>";
+echo $a / $b . "<br>";
+
+echo $a + $age . "<br>";
+print($age + 2);
+echo "<br>";
+
+var_dump($name);
+var_dump($age);
+var_dump($price);
+var_dump($isAvailable);
+
+?>
